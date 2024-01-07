cd564b8610fb0870f0eb06493698f72c8979da36
Dan Keenan <dk@dankeenan.org>
2024-01-07 10:06:37 -0500

    Create Browser and move functionality from Editor to Browser.

--- src/patchman/EditorWindow.h
+++ src/patchman/EditorWindow.h
@@ -24,21 +24,25 @@ class EditorWindow: public QMainWindow
 {
 Q_OBJECT
 public:
-    explicit EditorWindow(QWidget *parent = nullptr);
+    /**
+     * New EditorWindow.
+     *
+     * This window will take ownership of the passed @p rom.
+     * @param rom
+     * @param parent
+     */
+    explicit EditorWindow(Rom *rom, const QString &path, QWidget *parent = nullptr);
 
 private:
     RomEditor *editor_ = nullptr;
     struct Actions
     {
-        QMenu *fileNew = nullptr;
-        QAction *fileOpen = nullptr;
-        QMenu *fileRecent = nullptr;
         QAction *fileSave = nullptr;
         QAction *fileSaveAs = nullptr;
         QAction *fileCreateReport = nullptr;
-        QAction *fileExit = nullptr;
-        QAction* helpAbout = nullptr;
-        QAction* helpHomepage = nullptr;
+        QAction *fileClose = nullptr;
+        QAction *helpAbout = nullptr;
+        QAction *helpHomepage = nullptr;
     };
     Actions actions_;
     struct Widgets
@@ -54,10 +58,7 @@ private:
     void initWidgets();
 
     void saveTo(const QString &path);
-    void openFrom(const QString &path);
     void setSaveEnabled();
-    void updateRecentDocuments();
-    void replaceOpenRom(Rom *newRom);
     bool maybeSave();
     void updatePatchedRacksCount();
     void updateChecksum();
@@ -66,8 +67,6 @@ protected Q_SLOTS:
     void closeEvent(QCloseEvent *event) override;
 
 private Q_SLOTS:
-    void newFile(Rom::Type romType);
-    void open();
     void save();
     void saveAs();
     void createReport();
