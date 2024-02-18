deddfe2ad61f9aec00c6cab687d46c0e39e32aeb
Dan Keenan <dk@dankeenan.org>
2024-02-18 13:42:29 -0500

    Cleanup HTML documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -34,10 +34,20 @@ rst_prolog = """
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# https://github.com/executablebooks/sphinx-book-theme
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 html_theme_options = {
-    'navigation_with_keys': False
+    'navigation_with_keys': False,
+    'repository_url': 'https://github.com/danielskeenan/patchman',
+    'use_source_button': False,
+    'use_edit_page_button': False,
+    'use_repository_button': True,
+    'use_issues_button': True,
+    'use_download_button': False,
+    'use_fullscreen_button': False,
+    'path_to_docs': 'doc/',
+    'repository_branch': 'main',
 }
 
 # -- Options for QtHelp output -------------------------------------------------
