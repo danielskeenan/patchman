84543672b8a4ec322c95fc97916a387cac4184fd
Dan Keenan <dk@dankeenan.org>
2024-07-23 19:21:39 -0400

    Fix test runner to init Qt App.

--- test/patchlib/main.cpp
+++ test/patchlib/main.cpp
@@ -8,10 +8,14 @@
 
 #include <catch2/catch_session.hpp>
 #include <QtGlobal>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
     Q_INIT_RESOURCE(bin);
+
+    // Needed for tests that hit the database, see https://bugreports.qt.io/browse/QTBUG-117621
+    QCoreApplication app(argc, argv);
 
     return Catch::Session().run(argc, argv);
 }
